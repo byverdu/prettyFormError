f0ec9fea6455fd50e0559514f34ab4221efb2150
byverdu <byverdu@gmail.com>
2016-11-11 23:34:09 +0000

    unit tests for plugin constructor and jquery plugin initialization

--- mocha/tests.js
+++ mocha/tests.js
@@ -1,25 +1,36 @@
 /* global before, chai, describe, it, PrettyError */
 
-const expect = chai.expect;
-let prettyError;
-describe( 'PrettyError constructor', () => {
-  beforeEach(() => {
-    prettyError = new PrettyError();
+( function ( $ ) {
+  const expect = chai.expect;
+  let prettyError;
+  let jQueryMock;
+
+  describe( 'PrettyError constructor', () => {
+    beforeEach(() => {
+      prettyError = new PrettyError();
+    });
+    it( 'is defined', () => {
+      expect( PrettyError ).not.to.be.eql( undefined );
+    });
+    it( 'has a jQuery element property', () => {
+      expect( prettyError ).to.have.property( '$element' ).and.is.an.instanceOf( $ );
+    });
+    describe( 'prettyError jQuery plugin', () => {
+      beforeEach(() => {
+        jQueryMock = $( '.prettyError' ).prettyError();
+      });
+      it( 'is defined', () => {
+        const $div = $( '<div />' );
+        expect( jQueryMock ).to.have.property( 'prettyError' );
+      });
+      it( 'returns an array within the selectors', () => {
+        console.log(jQueryMock);
+        expect( jQueryMock ).to.have.length.least(2);
+      });
+      it( 'can be chainable', () => {
+        jQueryMock.addClass( 'noMore' );
+        expect( jQueryMock.attr( 'class') ).to.contain( 'noMore' );
+      });
+    });
   });
-  it( 'is defined', () => {
-    expect( PrettyError ).not.to.be.eql( undefined );
-  });
-  it( 'has a setSettings property', () => {
-    expect( prettyError ).to.have.property( 'setSettings' ).and.is.an.instanceOf( 'Function' );
-  });
-  it( 'invokes setSettings when is initialized', () => {
-    const spySetSettings = sinon.spy( prettyError, 'setSettings' );
-    expect( spySetSettings.callCount ).to.eq( 1 );
-  });
-  it( 'is called', () => {
-    // const mock = sinon.stub();
-    // mock.setColor = sinon.spy();
-    console.log(sinon);
-    expect( sinon ).not.to.be.eql( undefined );
-  });
-});
+}( jQuery ));
--- src/prettyError.js
+++ src/prettyError.js
@@ -1,37 +1,51 @@
 /* eslint-disable no-param-reassign */
 // prettyError jQuery plugin
 
-function PrettyError( element, options ) {
+function PrettyError( element ) {
   this.$element = $( element );
-  this.options = options || {};
-  this.setSettings( options );
 }
 
-PrettyError.prototype.setSettings = function () {
-  this.$element.css({
-    color: this.options.color,
-    background: this.options.background
-  });
-};
-
 ( function ( $ ) {
-  $.fn.prettyFormError = function ( options ) {
-    const opts = options || $.fn.prettyFormError.settings;
-    $.extend({}, new PrettyError( this, opts ));
-    return this;
+  $.fn.prettyError = function () {
+    $.extend({}, new PrettyError( this ));
+    return this.each(function() {
+        // Do something to each element here.
+    });;
   };
-
-  // Ability to set settings prior the plugin is initialized
-  $.fn.prettyFormError.settings = {
-    color: '',
-    background: ''
-  };
-  // $.fn.prettyFormError.settings.color = 'red';
-  // $.fn.prettyFormError.settings.background = 'purple';
-  //
-  // console.log($.fn.prettyFormError.settings);
 
 }( jQuery ));
+
+
+// function PrettyError( element, options ) {
+//   this.$element = $( element );
+//   this.options = options || {};
+//   this.setSettings( options );
+// }
+
+// PrettyError.prototype.setSettings = function () {
+//   this.$element.css({
+//     color: this.options.color,
+//     background: this.options.background
+//   });
+// };
+
+// ( function ( $ ) {
+//   $.fn.prettyFormError = function ( options ) {
+//     const opts = options || $.fn.prettyFormError.settings;
+//     $.extend({}, new PrettyError( this, opts ));
+//     return this;
+//   };
+//
+//   // Ability to set settings prior the plugin is initialized
+//   $.fn.prettyFormError.settings = {
+//     color: '',
+//     background: ''
+//   };
+//   $.fn.prettyFormError.settings.color = 'red';
+//   $.fn.prettyFormError.settings.background = 'purple';
+//
+//
+// }( jQuery ));
 
 /*
 
