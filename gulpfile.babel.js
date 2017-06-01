7cc7af4b42f2118336a35e34042f8f5977dc48ea
albert Byverdu :) <byverdu@gmail.com>
2017-06-01 21:58:23 +0100

    added setup file to remember gulp tasks <3

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,5 +2,6 @@ import gulp from 'gulp';
 require( 'require-dir' )( './build/tasks' );
 
 gulp.task( 'default', [ 'serve', 'watch' ]);
+gulp.task( 'prouction', 'build');
 gulp.task( 'tests', [ 'serve-test', 'watch' ]);
 gulp.task( 'docs', [ 'serve-docs', 'watch' ]);
