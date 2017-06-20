c320bf1822756a32e3b4177c7e1e53f95ee7cb39
albert Byverdu :) <byverdu@gmail.com>
2017-06-20 21:33:14 +0100

    renamed plugin files for jquery

--- src/jquery.prettyFormError.js
+++ src/jquery.prettyFormError.js
@@ -1,5 +1,5 @@
 /* global IprettyError */
-/* @flow */
+/*  */
 // prettyError jQuery plugin
 
 ;( function( $, window, document, undefined ) { // eslint-disable-line
@@ -7,7 +7,7 @@
   var dataKey = 'plugin_' + pluginName;
 
   // utils
-  function createErrorsForInvalid( invalid, options: IprettyError ) {
+  function createErrorsForInvalid( invalid, options ) {
     return $.each( invalid, function( index, value ) {
       var errors = $( '<' + options.elementError + '>' )
         .addClass( options.classError )
@@ -18,14 +18,14 @@
   }
 
   // Plugin constructor
-  var Plugin = function( element, options: IprettyError ) {
+  var Plugin = function( element, options ) {
     this.element = $( element );
     this.options = {
       multiCheckbox: {
         enabled: false,
         selector: '.multiCheckbox'
       },
-      classError: 'IprettyError',
+      classError: 'prettyFormError',
       positionMethod: 'after',
       elementError: 'div',
       callToAction: 'button',
@@ -37,7 +37,7 @@
   };
 
   Plugin.prototype = {
-    init: function( options: IprettyError ) {
+    init: function( options ) {
       // user options checker for positionMethod
       // to avoid breaking the plugin initialization
       if ( options !== undefined &&
@@ -63,7 +63,7 @@
 
     // workaround to validate the multiple
     // checkboxes that at least one value is required
-    multiCheckboxConfig: function( options: IprettyError ) {
+    multiCheckboxConfig: function( options ) {
       var allCheckboxes = $( options.multiCheckbox.selector );
       allCheckboxes
         .change( function() {
@@ -76,7 +76,7 @@
     },
 
     // button click handler
-    handleClickCallToAction: function( element, options: IprettyError ) {
+    handleClickCallToAction: function( element, options ) {
       var btn = element.find( options.callToAction );
 
       btn.on( 'click', function( event ) {
--- src/module.prettyFormError.js
+++ src/module.prettyFormError.js
@@ -1,67 +1,247 @@
-//      
-/* global PrettyFormError */
+// 
+/* global IprettyError */
 
-class PrettyError {
-                       
-                       
-                       
-                     
+/** Global factory for PrettyFormError
+ *  using vanilla JS
+ * @returns {Function} init(element, options)
+ */
+function PrettyFormError() {
+  /**
+   * Assignement a default value and checking valid one for positionMethod prop
+   * @param {string} [userValue] user value
+   * @returns {string} value for positionMethod prop
+   */
+  function _valuePositonChecker( userValue  = 'afterend' ) {
+    const notFound = [ 'beforebegin', 'afterend' ].indexOf( userValue.toLowerCase()) === -1;
 
-  constructor(
-    element             ,
-    options                           = {}
-  ) {
-    const {
-      callToAction, elementError, classError
-    } = options;
-
-    this.element = element;
-    this.callToAction = callToAction || 'button';
-    this.elementError = elementError || 'div';
-    this.classError = classError || 'prettyFormError';
+    if ( notFound ) {
+      console.warn( 'positionMethod prop value should be "beforebegin" or "afterend", a default "afterend" value has been assigned' );
+    }
+    return notFound ?
+      'afterend' :
+      userValue.toLowerCase();
   }
 
-  _removeOldErrors()       {
-    const oldErrors = document.querySelectorAll( `.${this.classError}` );
-    [].forEach.call( oldErrors, ( error              ) => {
-      error.remove();
+  /** Setting defualt properties values if user
+   *  doesn't specify them
+   * @param {IprettyError} options Object implementing IprettyError
+   * @returns {void}
+   */
+  function _setOpts( options ) {
+    const tempFadeOpt = {fadeOut: false, fadeOutOpts: ''};
+    const tempMulti = {enabled: false, selector: '.multiCheckbox'};
+    const callToAction = options.callToAction || 'button';
+    const elementError = options.elementError || 'div';
+    const classError = options.classError || 'prettyFormError';
+    const positionMethod = _valuePositonChecker( options.positionMethod );
+    const focusErrorOnClick = options.focusErrorOnClick || true;
+    const fadeOutError = options.fadeOutError || tempFadeOpt;
+    const multiCheckbox = options.multiCheckbox || tempMulti;
+
+    return {
+      callToAction,
+      elementError,
+      classError,
+      positionMethod,
+      focusErrorOnClick,
+      fadeOutError,
+      multiCheckbox
+    };
+  }
+
+  /** Converts actual element into array
+   * @param {HTMLElement} element Element/s selected by user
+   * @returns {Array<HTMLElement>} Elements into an array
+   */
+  function _convertToArray( element ) {
+    return ( element instanceof Element ) ?
+      [element] :
+      element;
+  }
+
+
+  /** Romoves old errors displayed in screen
+   * @param {EventTarget} element Current form that is validated
+   * @param {string} cssSelector css class name to serach and delete
+   * @return {void}
+   */
+  function _removeOldErrors( element, cssSelector ) {
+    if ( element ) {
+      const oldErrors = element.querySelectorAll( `.${cssSelector}` );
+      [].forEach.call( oldErrors, ( error ) => {
+        error.remove();
+      });
+    }
+  }
+
+  /**
+   * Creates HTML to hold the error for all invalid inputs
+   * @param {NodeList<HTMLElement>} invalids Invalid inputs for actual submited form
+   * @param {string} element User or default element defined for error
+   * @param {string} classError User or default css class for error
+   * @param {string} positionMethod User or default css class for error
+   * @return {void}
+   */
+  function _createErrorElement(
+    invalids,
+    element,
+    classError,
+    positionMethod
+  ) {
+    [].forEach.call( invalids, ( invalid ) => {
+      const tempElem = document.createElement( element );
+      tempElem.textContent = invalid.validationMessage;
+      tempElem.classList.add( `${classError}` );
+      invalid.insertAdjacentElement( positionMethod, tempElem );
     });
   }
 
-  _createErrorElement( element        , errorMsg         )              {
-    const tempElem = document.createElement( element );
-    tempElem.textContent = errorMsg;
-    tempElem.classList.add( this.classError );
-    return tempElem;
+  /**
+   * Applies click handler for collection
+   * @param {*} elements Array of HTMLElements
+   * @param {*} options User options or defaults
+   * @return {void}
+   */
+  function _clickHandlerNodeList( elements, options ) {
+    for ( let i = 0; i < elements.length; i++ ) {
+      _onClickHandler( elements[ i ], options );
+    }
   }
 
-  _onClickHandler()       {
-    const button               = this.element.querySelector( `${this.callToAction}` );
+  /**
+   * Set inputs values as empty string for those that are valid
+   * @param {NodeList<HTMLElement>} valids Valids inputs when form is submitted
+   * @return {void}
+   */
+  function _clearValidInputs( valids ) {
+    [].forEach.call( valids, ( valid ) => {
+      valid.value = '';
+    });
+  }
+
+  /**
+   * Adds CSS class with animation so error can fadeout
+   * @returns {MutationObserver} mutation observer constructor
+   */
+  function _fadeOutErrorConfig() {
+    return new MutationObserver( mutations => {
+      mutations.forEach( mutation => {
+        if ( mutation.addedNodes.length > 0 ) {
+          ( mutation.addedNodes[ 0 ] ).classList.add( 'prettyFormError-fade' );
+        }
+      });
+    });
+  }
+
+  /**
+   * setup for multi checkboxes that needs validation
+   * @param {string} cssSelector common css selector for all checkboxes
+   * @returns {void}
+   */
+  function _multiCheckboxConfig( cssSelector ) {
+    const checkboxes = document.querySelectorAll( cssSelector );
+
+    function changeHandler() {
+      const checkedCount = document.querySelectorAll( `${cssSelector}:checked` ).length;
+
+      if ( checkedCount > 0 ) {
+        for ( let i = 0; i < checkboxes.length; i++ ) {
+          checkboxes[ i ].removeAttribute( 'required' );
+        }
+      } else {
+        for ( let i = 0; i < checkboxes.length; i++ ) {
+          checkboxes[ i ].setAttribute( 'required', 'required' );
+        }
+      }
+    }
+
+    [].forEach.call( checkboxes, input => {
+      input.addEventListener( 'change', changeHandler );
+    });
+  }
+
+  /**
+   * Append click event for element within the form
+   * @param {HTMLElement} element form element to apply
+   * @param {*} options User options or defaults
+   * @return {void}
+   */
+  function _onClickHandler( element, options ) {
+    const button = element.querySelector( `${options.callToAction}` );
 
     if ( button ) {
-      button.addEventListener( 'click', ( event             ) => {
-        const invalid = this.element.querySelectorAll( ':invalid' );
-        if ( invalid.length > 0 ) {
-          event.preventDefault();
+      button.addEventListener( 'click', ( event ) => {
+        event.preventDefault();
+        const invalids = element.querySelectorAll( ':invalid' );
+        const valids = element.querySelectorAll( ':valid' );
 
-          this._removeOldErrors();
+        // removing old errors
+        if ( !options.fadeOutError.fadeOut && document.querySelector( `.${options.classError}` )) {
+          _removeOldErrors( element,  options.classError );
+        }
+        // fading old errors
+        if ( options.fadeOutError.fadeOut ) {
+          let observer = _fadeOutErrorConfig();
+          const config = { attributes: true, childList: true, characterData: true };
+          observer.observe( element, config );
 
-          // Append errors to those invalid fields
-          invalid.forEach(( input      ) => {
-            const elem = this._createErrorElement(
-              this.elementError,
-              ( input                   ).validationMessage
-            );
+          setTimeout(() => {
+            _removeOldErrors( element,  options.classError );
+          }, 6200 );
 
-            input.insertAdjacentElement( 'afterend', elem );
-          });
-          console.log( invalid );
+          // clearing observer
+          if ( invalids.length === 0 && valids.length > 0 ) {
+            observer.disconnect();
+            observer = null;
+          }
+        }
+
+        // adding new errors
+        if ( invalids.length > 0 ) {
+          _createErrorElement(
+            invalids,
+            options.elementError,
+            options.classError,
+            options.positionMethod
+          );
+        }
+
+        // clearing valid inputs
+        if ( invalids.length === 0 && valids.length > 0 ) {
+          _clearValidInputs( valids );
+        }
+
+        // focusing on first errrored input
+        if ( invalids.length > 0 && options.focusErrorOnClick ) {
+          invalids[ 0 ].focus();
+        }
+
+        // multiCheckbox configuration
+        if ( options.multiCheckbox.enabled ) {
+          _multiCheckboxConfig( options.multiCheckbox.selector );
         }
       });
     }
   }
 
-  init() {
-    this._onClickHandler();
-  }
+  return {
+    init: (
+      element,
+      options = {}
+ ) => {
+      const isHTMLElement = element instanceof Element ||
+        element instanceof NodeList ||
+        element instanceof HTMLCollection;
+      let tempElem;
+
+      // seting default element for empty case
+      isHTMLElement ?
+        tempElem = _convertToArray( element ) :
+        tempElem = document.querySelectorAll( 'form' );
+
+      // seting user props or default
+      // and adding click handler
+      _clickHandlerNodeList( tempElem, _setOpts( options ));
+    }
+  };
 }
